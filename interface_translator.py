4bf780a99aa8a96926b62190afb360fb7d758a2b
AndrewPiroli <45859969+AndrewPiroli@users.noreply.github.com>
2020-07-08 22:00:42 -0400

    Update copyright year

--- interface_translator.py
+++ interface_translator.py
@@ -1,7 +1,7 @@
 # Andrew Piroli
 # MIT License
 #
-# Copyright (c) 2018 AndrewPiroli
+# Copyright (c) 2018-2020 AndrewPiroli
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
