06362c9705f45dc0ee633f3b0c683de2d776a2b4
AndrewPiroli <45859969+AndrewPiroli@users.noreply.github.com>
2020-07-09 00:26:43 -0400

    switch to logging

--- interface_translator.py
+++ interface_translator.py
@@ -24,6 +24,7 @@
 
 import argparse
 import sys
+import logging
 
 
 class IntTranslate:
@@ -33,61 +34,61 @@ class IntTranslate:
         self.old_int = []
         self.new_int = []
         self.stripped_map = {}
+        logging.basicConfig(format="", level=logging.DEBUG)
+        self.log = logging.getLogger("IntTranslate")
+        self.log.setLevel(logging.CRITICAL)
 
     def discover(self):
         for idx, line in enumerate(self.config):
             if line.strip().startswith("interface "):
                 self.int_index.append(idx)
-                if self.args.debug:
-                    print(
-                        "func discover(config): new interface discovered: "
-                        + str(self.config[idx].strip())
-                    )
-                    print(
-                        "func discover(config): new interface discovered at idx: "
-                        + str(idx)
-                    )
+                self.log.debug(
+                    "func discover(config): new interface discovered: "
+                    + str(self.config[idx].strip())
+                )
+                self.log.debug(
+                    "func discover(config): new interface discovered at idx: "
+                    + str(idx)
+                )
 
     def open_config(self):
-        if self.args.debug:
-            print("func open_config(): attempting to open file at: " + self.args.input)
+        self.log.debug(
+            "func open_config(): attempting to open file at: " + self.args.input
+        )
         try:
             infile = open(self.args.input, "r")
             self.config = infile.readlines()
             infile.close()
         except Exception as e:
-            print("Error reading input file!")
-            print(e)
-            if self.args.debug:
-                print("error in open_config()")
-                print(repr(e))
-        if self.args.debug:
-            print("Configuration recieved successfully!")
-            print("Dumping config to console:")
-            print(self.config)
-            print()
+            self.log.critical("Error reading input file!")
+            self.log.critical(e)
+            self.log.debug("error in open_config()")
+            self.log.debug(repr(e))
+        self.log.debug("Configuration recieved successfully!")
+        self.log.debug("Dumping config to console:")
+        self.log.debug(self.config)
+        self.log.debug("")
 
     def save_config(self):
-        if self.args.debug:
-            print("func save_config(): attempting save to file: " + self.args.output)
+        self.log.debug(
+            "func save_config(): attempting save to file: " + self.args.output
+        )
         try:
             outfile = open(self.args.output, "w+", newline="\n")
             outfile.writelines(self.config)
             outfile.close()
         except Exception as e:
-            print("Error writing config")
-            print(e)
-            if self.args.debug:
-                print("error in save_config()")
-                print(repr(e))
+            self.log.critical("Error writing config")
+            self.log.critical(e)
+            self.log.debug("error in save_config()")
+            self.log.debug(repr(e))
 
     def trans_config(self):
-        if self.args.debug:
-            print("func trans_config(): stripped_map = " + str(self.stripped_map))
+        self.log.debug("func trans_config(): stripped_map = " + str(self.stripped_map))
         for idx in self.int_index:
             if self.config[idx].strip() in self.stripped_map:
-                print("Discovered interface inside existing map!")
-                print(
+                self.log.critical("Discovered interface inside existing map!")
+                self.log.critical(
                     self.config[idx].strip()
                     + " -> "
                     + self.stripped_map.get(self.config[idx].strip())
@@ -96,9 +97,9 @@ class IntTranslate:
                     self.stripped_map.get(self.config[idx].strip()) + "\n"
                 )
             else:
-                print("New interface discovered!")
+                self.log.critical("New interface discovered!")
                 self.old_int.append(self.config[idx].strip())
-                print(self.config[idx].strip())
+                self.log.critical(self.config[idx].strip())
                 answer = input(
                     "Enter new interface name or pass for passthru: interface "
                 ).strip()
@@ -107,62 +108,58 @@ class IntTranslate:
                 else:
                     self.config[idx] = "interface " + answer + "\n"
                     self.new_int.append("interface " + answer)
-        if self.args.debug:
-            print("func trans_config(): old_int: " + str(self.old_int))
-            print("func trans_config(): new_int: " + str(self.new_int))
+        self.log.debug("func trans_config(): old_int: " + str(self.old_int))
+        self.log.debug("func trans_config(): new_int: " + str(self.new_int))
 
     def save_map(self):
         if self.args.reverse:
-            print()
-            print("Warning!")
-            print("You have selected to reverse AND save the map!")
-            print(
+            self.log.critical("")
+            self.log.critical("Warning!")
+            self.log.critical("You have selected to reverse AND save the map!")
+            self.log.critical(
                 "Please confirm this is intended and the original map will not be overwritten"
             )
             if input("Is this OK (y/N): ").strip().lower().startswith("y"):
-                print("Saving map anyway!")
+                self.log.critical("Saving map anyway!")
             else:
-                print("Map save aborted!")
+                self.log.critical("Map save aborted!")
                 return
-        if self.args.debug:
-            print("func save_map(): attempting to save map at file: " + self.args.save)
+        self.log.debug(
+            "func save_map(): attempting to save map at file: " + self.args.save
+        )
         try:
             mapping = str(dict(zip(self.old_int, self.new_int)))
             mapfile = open(self.args.save, "w+", newline="\n")
             mapfile.write(mapping)
             mapfile.close()
         except Exception as e:
-            print("Error saving map file")
-            print(e)
-            if self.args.debug:
-                print("error in save_map()")
-                print(repr(e))
+            self.log.critical("Error saving map file")
+            self.log.critical(e)
+            self.log.debug("error in save_map()")
+            self.log.debug(repr(e))
 
     def load_map(self):
         self.stripped_map = None
-        if self.args.debug:
-            print("func load_map() attempting to open map at file: " + self.args.map)
+        self.log.debug(
+            "func load_map() attempting to open map at file: " + self.args.map
+        )
         try:
             mapfile = open(self.args.map, "r")
             map = mapfile.read()
-            if self.args.debug:
-                print("func load_map(): raw map text: " + map)
+            self.log.debug("func load_map(): raw map text: " + map)
             self.security_check(map)
             self.stripped_map = eval(map)
         except AssertionError as e:
             sys.exit(e)
         except Exception as e:
-            print("Error openening map")
-            print(e)
-            if self.args.debug:
-                print("Error in load_map()")
-                print(repr(e))
+            self.log.critical("Error openening map")
+            self.log.critical(e)
+            self.log.debug("Error in load_map()")
+            self.log.debug(repr(e))
         if not self.stripped_map:
-            if self.args.debug:
-                print("func load_map(): error loading map")
+            self.log.debug("func load_map(): error loading map")
             return {}
-        if self.args.debug:
-            print("func load_map(): stripped_map: " + str(self.stripped_map))
+        self.log.debug("func load_map(): stripped_map: " + str(self.stripped_map))
 
     @staticmethod
     def int_array_fixup(map):
@@ -194,8 +191,7 @@ class IntTranslate:
                 "Security: Map file failed security check! The map file should only contain a python dictionary, NOT code."
             )
         else:
-            if self.args.debug:
-                print("security_check(mapfile): Map pass security check.")
+            self.log.debug("security_check(mapfile): Map pass security check.")
 
     def main(self):
         parser = argparse.ArgumentParser()
@@ -210,13 +206,15 @@ class IntTranslate:
             "-r", "--reverse", help="Reverse the translation map", action="store_true"
         )
         self.args = parser.parse_args()
+        if self.args.debug:
+            self.log.setLevel(logging.DEBUG)
         self.open_config()
         self.discover()
         if self.args.map:
             self.load_map()
             if self.args.reverse:
                 if self.args.debug:
-                    print("main: reversing loaded map file")
+                    self.log.critical("main: reversing loaded map file")
                 rev_map = {v: k for k, v in self.stripped_map.items()}
                 self.stripped_map = rev_map
             fixup = self.int_array_fixup(self.stripped_map)
